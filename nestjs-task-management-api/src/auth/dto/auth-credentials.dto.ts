259bd4547da755763aa2ecb7e52f61cc14fc159e
ln0rd <leonardolsbufalo@gmail.com>
2022-02-08 21:06:01 -0300

    feat: unique username

--- nestjs-task-management-api/src/auth/dto/auth-credentials.dto.ts
+++ nestjs-task-management-api/src/auth/dto/auth-credentials.dto.ts
@@ -1,10 +1,16 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsString, Matches, MaxLength, MinLength } from 'class-validator';
 
 export class AuthCredentialsDto {
-  @IsNotEmpty()
   @IsString()
+  @MinLength(3)
+  @MaxLength(20)
   username: string;
 
-  @IsNotEmpty()
+  @MinLength(6)
+  @MaxLength(100)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'Password need 1 uppercase caracter, 1 lowercase caracter, 1 number and 1 special caracter',
+  })
   password: string;
 }
